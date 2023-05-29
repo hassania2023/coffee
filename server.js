f4013773bb36e5ccf7924e4a22dfb034f3a0ce24
OuaritHajar <ouarit.hjr@gmail.com>
2023-05-29 11:32:11 +0100

    commit2

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 'use strict';
 const express = require('express');
+const path = require('path');
+
 // Constants
 const PORT = 3000;
-const HOST = '0.0.0.0'; // Définissez l'adresse IP ou le nom d'hôte approprié
+const HOST = '0.0.0.0';
+
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Docker and Nodejs');
-});
+
+// Définir le répertoire contenant les fichiers statiques
+const publicDirectoryPath = path.join(__dirname, 'Coffe Website');
+
+// Configurer Express pour servir les fichiers statiques
+app.use(express.static(publicDirectoryPath));
+
 app.listen(PORT, HOST, () => {
   console.log(`Running on ${HOST}:${PORT}`);
 });
